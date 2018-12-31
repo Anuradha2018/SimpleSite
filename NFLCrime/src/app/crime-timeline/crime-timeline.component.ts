744acafff602b6e3e8f049aa646712bd5124e679
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-12-31 12:05:28 +0100

    a trial with chart.js using static data from API, done in thirty minutes

--- NFLCrime/src/app/crime-timeline/crime-timeline.component.ts
+++ NFLCrime/src/app/crime-timeline/crime-timeline.component.ts
@@ -11,15 +11,76 @@ import { Observable, throwError } from 'rxjs';
   styleUrls: ['./crime-timeline.component.css']
 })
 export class CrimeTimelineComponent implements OnInit {
-  chart = [];
+   chart = [];
 
   constructor(private crimeTimelineService: CrimeTimelineService) { }
 
+
   ngOnInit() {
-    this.crimeTimelineService.showTimeline()
+    let data = this.crimeTimelineService.showTimeline()
       .subscribe(res => {
         console.log(res);
       })
+      let ctx = document.getElementById("myChart");
+      let myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+      labels: ["Feb,2000", "July,2000", "Aug,2000", "Sept,2000", "Dec,2001", "oct,2002", "May,2004", "March,2005", "June,2006", "Feb,2011", "April,2011", "May,2011", "June,2011", "Oct,2014"],
+      datasets: [{
+      label: '# of arrest_count',
+      data: [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: false,
+    scales: {
+      xAxes: [{
+        ticks: {
+          maxRotation: 90,
+          minRotation: 80
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
   }
 
 }
